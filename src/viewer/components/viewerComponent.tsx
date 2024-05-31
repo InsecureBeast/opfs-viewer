0a56ab8b9be32685d11aa54a705c09f04c9e0ee8
dmitriev_pe <dmitriev_pe@ascon.ru>
2024-05-31 17:16:11 +0300

    styling text color

--- src/viewer/components/viewerComponent.tsx
+++ src/viewer/components/viewerComponent.tsx
@@ -90,11 +90,6 @@ export const Viewer: React.FC<IViewerProps> = (props) => {
 
   return (
   <div>
-    {/* <a href="#" className="py-2 px-4 inline-block text-center mb-3 rounded leading-5 text-gray-100 bg-indigo-500 border border-indigo-500 hover:text-white hover:bg-indigo-600 hover:ring-0 hover:border-indigo-600 focus:bg-indigo-600 focus:border-indigo-600 focus:outline-none focus:ring-0">
-      Add new 
-      { IconsRegistry.plusIcon }
-    </a> */}
-
     <nav aria-label="breadcrumb" className="mb-3">
       <ol className="flex flex-wrap justify-start bg-transparent text-lg font-semibold text-gray-600 mb-0 space-x-1 dark:text-gray-200">
         {
@@ -111,7 +106,7 @@ export const Viewer: React.FC<IViewerProps> = (props) => {
     </nav>
 
     <table className="table-auto min-w-full ltr:text-left rtl:text-right text-sm">
-      <thead className="dark:text-gray-500">
+      <thead className="text-gray-600 dark:text-gray-400">
         <tr>
           <th className="pr-4 py-3 font-normal text-left">Name</th>
           <th className="px-4 py-3 font-normal text-left">Date modified</th>
